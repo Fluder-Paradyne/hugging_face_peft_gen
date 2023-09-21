f4d96580ca6fc6c6a9e3e4f32eae2574d5d55d99
Ubuntu <ubuntu@ip-10-14-1-207.ec2.internal>
2023-09-21 12:38:57 +0000

    remove some packages

--- handler.py
+++ handler.py
@@ -2,13 +2,27 @@ import runpod
 import os
 import time
 from generation import inference
+from models import InputPayload
+import json
 
 
 ## load your model(s) into vram here
 
+
+
 def handler(event):
-    print(event,'event')
-    output = inference(event.input)
+    # print(event,'event')
+    # event = json.loads(event)
+    print(event["input"],'input')
+    model_input = event["input"]
+
+    # print(model_input.inputs, "model_input")
+    # model_input = json.loads(event["input"])
+    # print(model_input, "model_input")
+    input_payload = InputPayload(**model_input)
+    output = inference(input_payload)
+    print(output,'output')
+    return "hello world"
 
 runpod.serverless.start({
     "handler": handler
