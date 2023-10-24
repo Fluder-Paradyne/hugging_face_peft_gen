904eeb8d6d912d0b92f36c9ea37735f74dab53ce
rakesh-krishna-a-s <akrishna@contlo.com>
2023-10-24 11:00:15 +0530

    change input api

--- handler.py
+++ handler.py
@@ -6,19 +6,8 @@ from models import InputPayload
 import json
 
 
-## load your model(s) into vram here
-
-
 def handler(event):
-    # print(event,'event')
-    # event = json.loads(event)
-    print(event["input"], "input")
-    model_input = event["input"]
-
-    # print(model_input.inputs, "model_input")
-    # model_input = json.loads(event["input"])
-    # print(model_input, "model_input")
-    input_payload = InputPayload(**model_input)
+    input_payload = InputPayload(**event)
     output = inference(input_payload)
     print(output, "output")
     return output
